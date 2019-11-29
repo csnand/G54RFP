7639610d8fbcb7fb01fbb0e8a492918e3f66f3f2
csnand <askmryang@outlook.com>
2019-11-29 16:43:22 +0000

    dinning phil in progress

--- PartII/Code/dinningPhilosophers.hs
+++ PartII/Code/dinningPhilosophers.hs
@@ -5,6 +5,7 @@ import System.Random
 
 data Fork = MkFork (TMVar Int)
 
+
 newFork :: Int -> IO Fork
 newFork i = do
   fork <- newTMVarIO i
@@ -16,17 +17,17 @@ takeFork (MkFork fork) = takeTMVar fork
 putFork :: Int -> Fork -> STM ()
 putFork i (MkFork fork) = putTMVar fork i
 
-hungry :: String -> IO ()
-hungry name = putStrLn $ name ++ " is hungry."
+hungry :: String -> String
+hungry name = name ++ " is hungry."
 
-eating :: String -> Int -> Int -> IO ()
-eating name fork1 fork2 = putStrLn $ name ++
+eating :: String -> Int -> Int -> String
+eating name fork1 fork2 = name ++
   " with forks " ++ show fork1 ++
   " and " ++ show fork2 ++
   " is eating."
 
-thinking :: String -> IO ()
-thinking name =   putStrLn $ name ++ " is thinking."
+thinking :: String -> String
+thinking name =  name ++ " is thinking."
 
 philosophers :: [String]
 philosophers = ["Aristotle", "Kant", "Spinoza", "Marx", "Russel"]
@@ -34,33 +35,54 @@ philosophers = ["Aristotle", "Kant", "Spinoza", "Marx", "Russel"]
 
 randomDelay :: IO ()
 randomDelay = do
-  waitTime <- randomRIO (5,20)
+  waitTime <- randomRIO (1,3)
   threadDelay (waitTime * 1000000)
 
+putBuf :: TMVar [String] -> String -> STM ()
+putBuf buf str = do
+  xs <- readTMVar buf
+  putTMVar buf (xs ++ [str])
 
-runPhilosopher :: String -> (Fork, Fork) -> IO ()
-runPhilosopher name (left, right) = forever $ do
-  hungry name
+getBuf :: TMVar [String] -> STM String
+getBuf buf = do
+  xs <- readTMVar buf
+  case xs of
+    [] -> retry
+    (x:xs') -> do
+      putTMVar buf xs'
+      return x
+
+printBuf :: TMVar[String] -> IO ()
+printBuf buf = do
+  str <- atomically $ getBuf buf
+  putStrLn str
+  printBuf buf
+
+newInfoBuf :: IO (TMVar [String])
+newInfoBuf = newTMVarIO []
+
+dinning :: TMVar [String] -> String -> (Fork, Fork) -> IO ()
+dinning buf name (left, right) = forever $ do
+  atomically $ putBuf buf (hungry name)
   (leftFork, rightFork) <- atomically $ do
     leftFork <- takeFork left
     rightFork <- takeFork right
     return (leftFork, rightFork)
-  eating name leftFork rightFork
+  atomically $ putBuf buf (eating name leftFork rightFork)
   randomDelay
 
   atomically $ do
     putFork leftFork left
     putFork rightFork right
-  thinking name
+  atomically $ putBuf buf (thinking name)
   randomDelay
-
 
 main = do
   forks <- mapM newFork [1..5]
-  let namedPhilosophers     = map runPhilosopher philosophers
-      forkPairs             = zip forks (tail . cycle $ forks)
-      philosophersWithForks = zipWith ($) namedPhilosophers forkPairs
-  putStrLn "Running the philosophers. Press enter to quit."
-  mapM_ forkIO philosophersWithForks
+  infoBuf <- newInfoBuf
+  let dinningPhil       = map (dinning infoBuf) philosophers
+      forkPairs         = zip forks (tail . cycle $ forks)
+      dinningWithForks  = zipWith ($) dinningPhil (take 5 forkPairs)
+  mapM_ forkIO dinningWithForks
+  printBuf infoBuf
 
-  getLine
