c9832899af56f3e8ca5da1db47634d895376ca4b
csnand <askmryang@outlook.com>
2019-12-09 23:02:31 +0000

    part II in progress

---
+++ PartII/Report/auto/Report.el
@@ -0,0 +1,29 @@
+(TeX-add-style-hook
+ "Report"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "margin=0.75in") ("biblatex" "style=numeric")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "cite"
+    "inputenc"
+    "setspace"
+    "geometry"
+    "biblatex"
+    "float"
+    "graphicx")
+   (LaTeX-add-labels
+    "fig:dinning1"
+    "fig:dinning2"
+    "fig:bounded"
+    "fig:taskI.5.3")
+   (LaTeX-add-bibliographies
+    "../bibs/ref"))
+ :latex)
+
