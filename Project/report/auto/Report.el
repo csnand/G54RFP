d91cbc583e03ac3f3bfd91385f8dd75b8af4607e
csnand <askmryang@outlook.com>
2020-01-14 21:58:06 +0000

    report init

---
+++ Project/report/auto/Report.el
@@ -0,0 +1,23 @@
+(TeX-add-style-hook
+ "Report"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "margin=0.75in") ("biblatex" "style=numeric")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "inputenc"
+    "setspace"
+    "geometry"
+    "biblatex"
+    "float"
+    "graphicx")
+   (LaTeX-add-bibliographies
+    "../bibs/ref"))
+ :latex)
+
